907d43ea495445e29ad6abcf952ff4822db8fbab
Victor Bachmann Lueders <victorlueders@hotmail.com>
2024-07-25 18:05:35 -0300

    controllet e img size

---
+++ MVC_Curso/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Curso.Repositories.Interfaces;
+
+namespace MVC_Curso.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
